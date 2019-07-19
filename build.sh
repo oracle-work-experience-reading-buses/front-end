2ebdf28086135716933309868e6da3e1be26204b
Hyprxz <88142@farnborough.ac.uk>
2019-07-19 11:05:22 +0100

    used local services

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 docker build -t apache-image:latest .
-docker tag apache-image:latest lhr.ocir.io/aiapps/buses/apache:1.0.0
-docker push lhr.ocir.io/aiapps/buses/apache:1.0.0
+docker tag apache-image:latest lhr.ocir.io/aiapps/buses/apache:1.0.1
+docker push lhr.ocir.io/aiapps/buses/apache:1.0.1
+kubectl apply -f container.yaml
